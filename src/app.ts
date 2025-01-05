b7b461dd6f92abf0983c24b4eec27ce505ae1de5
faisal shahzad <faisal712000@gmail.com>
2025-01-05 10:23:50 +0500

    completed

---
+++ src/app.ts
@@ -0,0 +1,60 @@
+import { Header } from "./header";
+import { SocketUDP } from "./socket";
+import { ParseResponse } from "./parse_response";
+
+ export class Execute {
+    header: Header
+    socket: SocketUDP
+    parseresponse: ParseResponse
+    nameServerResponse: any
+
+    public constructor() {
+        this.header = new Header();
+        this.socket = new SocketUDP();
+        this.parseresponse = new ParseResponse();
+        this.nameServerResponse = "";
+    }
+
+    public execution(name: string): string {
+        // const x = this.header.toBinaryString("dns.google.com");
+        console.log("Resolving DNS for: ", name)
+        const x = this.header.toBinaryString(name);
+
+        this.socket.DnsQuery = x;
+        let answer = "";
+        //promise from socketUDP class
+        this.socket.sendQueryToRootServer().then((response) => {
+            this.nameServerResponse = response
+            // this.parseresponse.DnsResponse = this.nameServerResponse
+            // this.parseresponse.extractAnswerRecords();
+            answer = response;
+        }).catch ((error) => {
+            console.log("Resolving DNS Query Failed");
+            console.log(error);
+        });
+
+        return answer;
+    }
+}
+
+
+
+//script to take input from user and resolve dns
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter URL: ', (userInput: string) => {
+  const exe = new Execute();
+  exe.execution(userInput);
+  rl.close();
+});
+
+// const exe = new Execute();
+// exe.execution("dns.google.com");
+// exe.execution("www.urbanladder.com");
+// exe.execution("images.google.com");
+// exe.execution("www.google.com");
